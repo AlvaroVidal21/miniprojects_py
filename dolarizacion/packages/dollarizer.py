bbadaa7e203a9f484dd0382fe9c704a0478cc349
AlvaroVidal <alvaro.vidal1@unmsm.edu.pe>
2023-12-08 21:46:22 -0500

    Fix currency conversion bug

--- dolarizacion/packages/dollarizer.py
+++ dolarizacion/packages/dollarizer.py
@@ -4,6 +4,6 @@ DOLLAR = 3.74
 def dollarizer(list_of_clients):
     """Converts a client's balance from soles to dollars."""
     for client in list_of_clients:
-        client.amount = client.amount * DOLLAR
+        client.amount = client.amount / DOLLAR
 
     return list_of_clients
